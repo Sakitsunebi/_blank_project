b93057545531b74a9f79758cffe085c656afe740
Sakitsunibi <sakitsunebi@gmail.com>
2015-10-27 05:45:32 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+(function(){
+
+	document.addEventListener("DOMContentLoaded", function(event) {
+
+		var appendContainer = document.getElementById("appendContainer");
+
+		appendContainer.innerHTML = "Scripts have loaded... IIFE ran."
+
+	});	
+
+}(document));
